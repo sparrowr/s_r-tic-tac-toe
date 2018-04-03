ccc83fb8f8f93ff319bd257ce553e421af155445
sparrowr <sparrow@srubin.org>
2018-04-03 11:47:31 -0400

    displaying game information works!
    at this point all requirements are met except the UI ones, and the UI
    is also very ugly. shifting gears to fix that next. :D

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -1,47 +1,49 @@
 'use strict'
 
 const store = require('../store')
+const failureColor = '#933'
+const successColor = '#393'
 
 const signUpSuccess = function () {
   $('#message').text('Successfully signed up')
-  $('#message').css('background-color', '#cfc')
+  $('#message').css('background-color', successColor)
 }
 
 const signUpFailure = function () {
   $('#message').text('Failure signing up')
-  $('#message').css('background-color', '#f99')
+  $('#message').css('background-color', failureColor)
 }
 
 const signInSuccess = function (data) {
   $('#message').text('Successfully signed in')
-  $('#message').css('background-color', '#cfc')
+  $('#message').css('background-color', successColor)
   store.user = data.user
 }
 
 const signInFailure = function () {
   $('#message').text('Failure signing in')
-  $('#message').css('background-color', '#f99')
+  $('#message').css('background-color', failureColor)
 }
 
 const changePasswordSuccess = function (data) {
   $('#message').text('Successfully changed password')
-  $('#message').css('background-color', '#cfc')
+  $('#message').css('background-color', successColor)
 }
 
 const changePasswordFailure = function () {
   $('#message').text('Failure changing password')
-  $('#message').css('background-color', '#f99')
+  $('#message').css('background-color', failureColor)
 }
 
 const signOutSuccess = function () {
   $('#message').text('Successfully signed out!')
-  $('#message').css('background-color', '#cfc')
+  $('#message').css('background-color', successColor)
   store.user = null
 }
 
 const signOutFailure = function () {
   $('#message').text('Failure signing out!')
-  $('#message').css('background-color', '#f99')
+  $('#message').css('background-color', failureColor)
 }
 
 module.exports = {
--- assets/scripts/game/ui.js
+++ assets/scripts/game/ui.js
@@ -1,32 +1,59 @@
 'use strict'
 
+const failureColor = '#933'
+const successColor = '#393'
+
 const createGameFailure = function () {
   $('#message').text('Failure creating game in API!')
-  $('#message').css('background-color', '#f99')
+  $('#message').css('background-color', failureColor)
 }
 
 const updateGameSuccess = function () {
   $('#message').text('Successfully updated game')
-  $('#message').css('background-color', '#cfc')
+  $('#message').css('background-color', successColor)
 }
 
 const updateGameFailure = function () {
   $('#message').text('Failure updating game')
-  $('#message').css('background-color', '#f99')
+  $('#message').css('background-color', failureColor)
 }
 
 const showIndexSuccess = function (data) {
   $('#message').text('Retrieved games!')
-  $('#message').css('background-color', '#cfc')
+  $('#message').css('background-color', successColor)
+  console.log(data)
+  console.log(data.games[0])
   $('#game-board').text('Your game history: ')
-  for (let i = 0; i < data.length; i++) {
-    $('#game-board').append(data[i])
+  for (let i = 0; i < data.games.length; i++) {
+    console.log(i)
+    console.log(data.games[i])
+    console.log(data.games[i].id, data.games[i].cells, data.games[i].over)
+    $('#game-board').append('<br>')
+    const gameTitle = 'Game ID: ' + data.games[i].id
+    $('#game-board').append('<h2>' + gameTitle + '</h2>')
+    $('#game-board').append('<p>Board: </p>')
+    for (let j = 0; j < 3; j++) {
+      let row = '|'
+      for (let k = 0; k < 3; k++) {
+        if (data.games[i].cells[j * 3 + k] !== '') {
+          row = row + data.games[i].cells[j * 3 + k] + '|'
+        } else {
+          row = row + '_|'
+        }
+      }
+      $('#game-board').append('<p>', row, '</p>')
+    }
+    if (data.games[i].over) {
+      $('#game-board').append('<p>', 'game is complete', '</p>')
+    } else {
+      $('#game-board').append('<p>', 'game is not complete', '</p>')
+    }
   }
 }
 
 const showIndexFailure = function () {
-  $('#message').text('Failure updating game')
-  $('#message').css('background-color', '#f99')
+  $('#message').text('Failure finding games')
+  $('#message').css('background-color', failureColor)
 }
 
 module.exports = {
